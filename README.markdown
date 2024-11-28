090c5d0ac418f98bc17ef0f46bec0d3055a5c2b4
Matt Hoffman <matt@matthoffman.org>
2024-11-28 10:48:38 -0500

    fixes:shebang in setup_env.sh, tmux.conf location in tmux.conf, and making setup script more verbose

--- README.markdown
+++ README.markdown
@@ -3,7 +3,7 @@ Intro
 
 This repo has gone through a lot of incarnations over the years, but at the moment it's 
 just a random assortment of config files I tend to use on any workstation. My zsh config
-is in [zprezto](https://github.com/matthoffman/oh-my-zsh), and I've been using Spacemacs
-for emacs config on and off for a while. 
+is in [its own repo](https://github.com/matthoffman/zdotdir), and I've been using 
+Spacemacs or similar kitchen-sink emacs configs on and off for a while. 
 
 This is the assorted extra bits. 
--- setup_env.sh
+++ setup_env.sh
@@ -1,27 +1,42 @@
-in/bash
+#!/bin/bash
 
 mkdir -p "$HOME/.config"
 if [ ! -d "$HOME/.config/zsh" ]; then
-        git clone --depth=1 --recursive https://github.com/matthoffman/zdotdir.git "$HOME/.config/zsh"
+    echo ".config/zsh does not yet exist; cloning repo"
+    git clone --depth=1 --recursive https://github.com/matthoffman/zdotdir.git "$HOME/.config/zsh"
+else
+    echo ".config/zsh already exists, not cloning repo"
 fi
 
 if [ ! -f "$HOME/.zshenv" ]; then
-        ln -s "$HOME/.config/zsh/.zshenv" "$HOME/.zshenv"
+    echo ".zshenv does not exist. Linking to $HOME/.config/zsh/.zshenv"
+    ln -s "$HOME/.config/zsh/.zshenv" "$HOME/.zshenv"
+else
+    echo ".zshenv  already exists, not replacing with $HOME/.config/zsh/.zshenv"
 fi
 
 _Z=$(which zsh)
 if [ $? -eq 0 ]; then
-        chsh -s "${_Z}"
+    echo "zsh is installed at $_Z, setting this as default shell"
+    chsh -s "${_Z}"
 else
-        echo "zsh is not installed, not changing shells"
+    echo "zsh is not installed, not changing shells"
 fi
 
 if [ ! -d "$HOME/.dotfiles" ]; then
-        git clone --depth=1 --recursive https://github.com/matthoffman/dotfiles.git "$HOME/.dotfiles"
+    echo ".dotfiles does not yet exist; cloning repo"
+    git clone --depth=1 --recursive https://github.com/matthoffman/dotfiles.git "$HOME/.dotfiles"
+else
+    echo ".dotfiles already exists, not cloning repo"
 fi
 
 mkdir -p "$HOME/.config/tmux"
 
 if [ ! -f "$HOME/.config/tmux/tmux.conf" ]; then
-        ln -s "${HOME}/.dotfiles/etc/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+    echo ".config/tmux/tmux.conf does not yet exist; linking to ${HOME}/.dotfiles/etc/tmux.conf"
+    ln -s "${HOME}/.dotfiles/etc/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+else
+    echo ".config/tmux/tmux.conf already exists, not linking to ${HOME}/.dotfiles/etc/tmux.conf"
 fi
+
+echo "Setup script completed"
